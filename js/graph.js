35560f5511a49774334941c5dd465b3de28e465c
Jorge Cruz <jlcruz7125@gmail.com>
2023-05-06 14:23:07 -0400

    Adding responsiveness to graph.html

--- js/graph.js
+++ js/graph.js
@@ -5,6 +5,22 @@ function onBackButtonPressed() {
     window.location.href = homeBaseUrl + type;
 }
 
+function setTitle(value){
+    const titleDesk = document.getElementById('title-desktop');
+    const titleMobl = document.getElementById('title-mobile');
+
+    titleDesk.innerText = 'Función ' + value;
+    titleMobl.innerText = 'Función ' + value;
+}
+
+function setInformation(value){
+    const informationDesk = document.getElementById('information-desktop');
+    const informationMobl = document.getElementById('information-mobile');
+
+    informationDesk.innerHTML = value;
+    informationMobl.innerHTML = value;
+}
+
 function getMainPageGraph() {
 
     const type = getQueryStringType();
@@ -16,36 +32,43 @@ function getMainPageGraph() {
 
     if (type == sineKey) {
         equationText.innerText = "\\(f(x) = sin{(x)}\\)"
+        setTitle('Seno');
         MakeGraph(sineGraph);
     }
 
     else if (type == cosineKey) {
         equationText.innerText = "\\(f(x) = cos{(x)}\\)"
+        setTitle('Coseno');
         MakeGraph(cosineGraph);
     }
 
     else if (type == tangentKey) {
         equationText.innerText = "\\(f(x) = tan{(x)}\\)"
+        setTitle('Tangente');
         MakeGraph(tangentGraph);
     }
 
     else if (type == cosecantKey) {
         equationText.innerText = "\\(f(x) = csc{(x)}\\)"
+        setTitle('Cosecante');
         MakeGraph(cosecantGraph);
     }
 
     else if (type == secantKey) {
         equationText.innerText = "\\(f(x) = sec{(x)}\\)"
+        setTitle('Secante');
         MakeGraph(secantGraph);
     }
 
     else if (type == cotangentKey) {
         equationText.innerText = "\\(f(x) = cot{(x)}\\)"
+        setTitle('Cotangente');
         MakeGraph(cotangentGraph);
     }
 
     else {
         equationText.innerText = "\\(f(x) = sin{(x)}\\)"
+        setTitle('Seno');
         MakeGraph(sineGraph);
     }
 }
